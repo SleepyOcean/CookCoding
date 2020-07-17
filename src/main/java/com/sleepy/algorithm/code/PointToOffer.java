3b1a9f849a1566612f379a78df8cd89da4917c8e
gehoubao <gehoubao@kedacom.com>
2020-07-17 16:48:15 +0800

    add:『剑指Offer』刷题集

---
+++ src/main/java/com/sleepy/algorithm/code/PointToOffer.java
@@ -0,0 +1,24 @@
+package com.sleepy.algorithm.code;
+
+/**
+ * 剑指Offer刷题集
+ *
+ * @author gehoubao
+ * @create 2020-07-17 16:05
+ **/
+public class PointToOffer {
+    public static void main(String[] args) {
+        test03();
+    }
+
+    private static void test03() {
+        System.out.println(findRepeatNumber(new int[]{2, 3, 1, 0, 2, 5, 3}));
+    }
+
+    public static int findRepeatNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            //todo
+        }
+        return 0;
+    }
+}
