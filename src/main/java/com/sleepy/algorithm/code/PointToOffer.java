15769588ebe112f57db2f97b4eb8f0fd9b92f85b
gehoubao <gehoubao@kedacom.com>
2020-07-21 20:04:01 +0800

    add:刷题集，+2

--- src/main/java/com/sleepy/algorithm/code/PointToOffer.java
+++ src/main/java/com/sleepy/algorithm/code/PointToOffer.java
@@ -1,8 +1,6 @@
 package com.sleepy.algorithm.code;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * 剑指Offer刷题集
@@ -12,7 +10,60 @@ import java.util.Stack;
  **/
 public class PointToOffer {
     public static void main(String[] args) {
-        test06();
+        test07();
+    }
+
+    /**
+     * 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+     * <p>
+     * 例如，给出
+     * 前序遍历 preorder = [3,9,20,15,7]
+     * 中序遍历 inorder = [9,3,15,20,7]
+     * 返回如下的二叉树：
+     * 3
+     * / \
+     * 9  20
+     * /  \
+     * 15   7
+     *  
+     * 限制：
+     * 0 <= 节点个数 <= 5000
+     */
+    private static void test07() {
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        Map<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
+        int length = preorder.length;
+        for (int i = 0; i < length; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        TreeNode root = buildTree(0, 0, length - 1, indexMap, preorder);
+        return root;
+    }
+
+    public TreeNode buildTree(int preRootIndex, int inorderStart, int inorderEnd, Map<Integer, Integer> indexMap, int[] preorder) {
+        if (inorderStart > inorderEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preRootIndex]);
+
+        root.left = buildTree(preRootIndex + 1, inorderStart + 1, indexMap.get(preRootIndex) - 1, indexMap, preorder);
+//        root.right = buildTree(preRootIndex + (indexMap.get(preRootIndex) -0));
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
     }
 
     /**
