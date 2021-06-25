d53d303190daec11ffab4069dfe3647cfc77fe86
sleepyocean <2913815267@qq.com>
2021-06-25 14:02:35 +0800

    update better solution for question No.11

--- Python/Solution11.py
+++ Python/Solution11.py
@@ -3,18 +3,15 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        m = 0
-        n = 1
-        size = len(height)
-        max = 0
-        while(m < n and n < size):
-            while(n < size):
-                newMax = (n - m) * min(height[m], height[n])
-                max = max(max, newMax)
-                n += 1
-            m += 1
-            n = m + 1
-        return max
+        m, n, res = 0, len(height) - 1, 0
+        while(m < n):
+            if height[m] < height[n]:
+                res = max(res, height[m] * (n - m))
+                m += 1
+            else:
+                res = max(res, height[n] * (n - m))
+                n -= 1
+        return res
 
 
 solution = Solution()
